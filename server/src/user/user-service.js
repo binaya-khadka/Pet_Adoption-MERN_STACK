0698cf20447a7c8929dbdcf7da699f48b59c4799
binaya-kazi <binayakhadka.kazi@gmail.com>
2024-10-02 13:36:28 +0545

    Clean code

--- server/src/user/user-service.js
+++ server/src/user/user-service.js
@@ -1,5 +1,5 @@
-import * as userRepository from './user-repository.js'
-import bcrypt from 'bcryptjs'
+import * as userRepository from './user-repository.js';
+import bcrypt from 'bcryptjs';
 import { jwtUtils } from '../utils/index.js';
 
 export async function getAllUser() {
@@ -12,8 +12,8 @@ export async function register(payload) {
   const userdata = {
     id: user.id,
     name: user.name,
-    email: user.email,
-  }
+    email: user.email
+  };
 
   const token = jwtUtils.generateToken(user._id);
 
@@ -24,21 +24,22 @@ export async function login({ email, password }) {
   const user = await userRepository.fetchUserByEmail(email);
 
   if (!user) {
-    throw { message: 'User not found' }
+    throw { message: 'User not found' };
   }
 
   if (!bcrypt.compareSync(password, user.password)) {
     throw {
-      message: "Invalid Credentials",
-    }
+      message: 'Invalid Credentials'
+    };
   }
 
   const token = jwtUtils.generateToken(user._id);
+
   const userData = {
     id: user.id,
     name: user.name,
-    email: user.email,
-  }
+    email: user.email
+  };
 
   return { user: userData, token };
 }
