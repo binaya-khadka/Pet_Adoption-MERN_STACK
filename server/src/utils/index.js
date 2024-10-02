a9bd03861576c758a5dca1d17b7af011413680eb
binaya-kazi <binayakhadka.kazi@gmail.com>
2024-10-02 13:40:27 +0545

    http status added

--- server/src/utils/index.js
+++ server/src/utils/index.js
@@ -1,3 +1,4 @@
-export * as apiMethodUtils from './apiMethod.js'
-export * as jwtUtils from './jwt.js'
-export { upload } from './upload.js'
+export * as apiMethodUtils from './apiMethod.js';
+export * as jwtUtils from './jwt.js';
+export { upload } from './upload.js';
+export * as httpStatus from './httpStatus.js';
